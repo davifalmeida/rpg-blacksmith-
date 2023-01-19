fc3764d2867fe82e7b4f6a899b211ae8acc56f28
Davi <davifalmeida@gmail.com>
2023-01-19 17:55:00 -0300

    req06

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -9,6 +9,9 @@ const createProducts = async (req: Request, res: Response) => {
 
 const getAllProducts = async (_req: Request, res: Response) => {
   const result = await productsServices.getAllProducts();
+  if (result.length === 0) {
+    return res.status(404).json({ message: 'Products not found' });
+  }
   res.status(200).json(result);
 };
   
--- src/routes/products.router.ts
+++ src/routes/products.router.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 
 import productsController from '../controllers/products.controller';
+import productsMiddleware from '../middlewares/products.middleware';
 
 const router = Router();
 
-router.post('/', productsController.createProducts);
+router.post(
+  '/', 
+  productsMiddleware.nameValidation,
+  productsMiddleware.amountValidation,
+  productsController.createProducts,
+);
 router.get('/', productsController.getAllProducts);
 
 export default router;
